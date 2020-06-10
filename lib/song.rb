17b87dc0223969e9057a5f04c5404b12b784727e
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-10 15:14:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,17 @@
 class Song
   attr_accessor :artist, :name, :genre
 
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-
-
-  def self.all
-    @@all
-  end
+  # @@all = []
+  #
+  # def initialize(name)
+  #   @name = name
+  #   @@all << self
+  # end
+  #
+  #
+  #
+  # def self.all
+  #   @@all
+  # end
 
 end
