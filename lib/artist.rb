ebe8d248362515712574447cad76050477bbc28d
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-10 05:02:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,13 +7,13 @@ class Artist
 
   end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-  #
-  # def songs
-  #   Song.all.select {|song| song.artist == self}
+  # def add_song(song)
+  #   @songs << song
+  #   song.artist = self
   # end
+  #
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 
 end
