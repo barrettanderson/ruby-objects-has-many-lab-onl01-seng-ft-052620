3c88dda6ed25f718f16f6ef99096a32aab4ddf5c
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-10 05:52:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,15 +3,15 @@ class Song
 
   @@all = []
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-
-  #
-  # def self.all
-  #   @@all
+  # def initialize(name)
+  #   @name = name
+  #   @@all << self
   # end
+
+
+
+  def self.all
+    @@all
+  end
 
 end
