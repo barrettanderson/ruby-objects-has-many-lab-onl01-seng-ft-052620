94b80efa67d42fdd1d2bdcacbe94e494bb23ea6c
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-10 05:53:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,8 +23,8 @@ class Artist
   #   @@song_count
   # end
   # #
-  # def songs
-  #   Song.all.select {|song| song.artist == self}
-  # end
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 
 end
