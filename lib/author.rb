a19333c05c6206659606897f8c75420366ed1fa7
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-10 16:19:00 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,11 +3,12 @@ class Author
   attr_accessor :name
   def initialize(name)
     @name = name
-    # @posts = []
+    @posts = []
   end
 
   def add_post(post)
-
+    @posts << post
+    post.author = self
   end
 
 end
