d28d8d93c75ffd2cccbdb5c809aeb3cae86464a7
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-10 15:31:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,12 +6,12 @@ class Artist
     @name = name
     @songs = []
   end
-  # #
-  # def add_song(song)
-  #   @songs << song
-  #   song.artist = self
-  #   @@song_count += 1
-  # end
+
+  def add_song(song)
+    # @songs << song
+    # song.artist = self
+    # @@song_count += 1
+  end
   #
   # def add_song_by_name(song)
   #   song = Song.new(song)
